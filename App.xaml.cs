dd6a9f5be1200a08da11092f3c8403a1e0604cc5
Deniss Kuzmins <deniss.kuzmins@elev.ga.ntig.se>
2025-02-10 14:57:47 +0100

    Tränat på event och name

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace prog1_wpf_TE23E_Kuzmins_Deniss;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
