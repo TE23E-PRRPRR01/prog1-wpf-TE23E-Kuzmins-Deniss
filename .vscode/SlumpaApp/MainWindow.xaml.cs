1688c132dfe4e88e38e57f630ef17ff9aeb8007d
Deniss Kuzmins <deniss.kuzmins@elev.ga.ntig.se>
2025-02-12 15:02:41 +0100

    Tranäat xaml och TryParse

---
+++ .vscode/SlumpaApp/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SlumpaApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    private void KlickSlumpa(object sender, RoutedEventArgs e)
+    {
+        if(int.TryParse(MaxVärdettxb.Text, out int MaxVärde) && MaxVärde > 0)
+        {
+                int slumptal = new Random().Next(1, MaxVärde+1); // Skapar ett slumpmässigt tal
+        Resultattxb.Text = slumptal.ToString();   // Uppdaterar TextBox med rätt namn
+
+        
+        } else{
+            Resultattxb.Text ="False!";
+        }
+    }
+}
